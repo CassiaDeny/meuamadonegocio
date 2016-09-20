95fb1cc003a2f832196fa866901885fe522c3570
cassiadeny <cassia.deny@gmail.com>
2016-09-20 14:25:12 -0300

    Merge do repositório privado, para corrigir a linha de tempo / ações de contribuições no repositório
    Log de contribuições:
    
    Autor: cassiadeny <cassia.deny@gmail.com>
    Data: terça-feira, 20 de setembro de 2016 13:28:26
    Remoção arquivo não utilizado no servidor de produção
    
    Autor: cassiadeny <cassia.deny@gmail.com>
    Data: terça-feira, 20 de setembro de 2016 13:06:43
    Nova forma de roteamento
    
    Autor: Rodrigo Miyashiro <rmdevwork@gmail.com>
    Data: terça-feira, 20 de setembro de 2016 11:50:25
    Agora e possivel acessar as materias a clicando no titulo da materia na home
    
    Autor: cassiadeny <cassia.deny@gmail.com>
    Data: terça-feira, 20 de setembro de 2016 10:42:39
    Correção do package.json
    
    Autor: cassiadeny <cassia.deny@gmail.com>
    Data: terça-feira, 20 de setembro de 2016 09:29:08
    Merge branch 'master' of https://git.heroku.com/devmeuamadonegocio
    
    Autor: cassiadeny <cassia.deny@gmail.com>
    Data: terça-feira, 20 de setembro de 2016 09:26:06
    Correção de texto da matéria.
    
    Autor: Rodrigo Miyashiro <rmdevwork@gmail.com>
    Data: terça-feira, 20 de setembro de 2016 01:58:23
    Google Analytics adicionado.
    
    Autor: Rodrigo Miyashiro <rmdevwork@gmail.com>
    Data: terça-feira, 20 de setembro de 2016 01:30:47
    Merge e adicionado Google Analytics
    
    Autor: Rodrigo Miyashiro <rmdevwork@gmail.com>
    Data: terça-feira, 20 de setembro de 2016 01:10:47
    Adicionado GoogleAnalytics em todas as paginas
    
    Autor: cassiadeny <cassia.deny@gmail.com>
    Data: terça-feira, 20 de setembro de 2016 01:06:10
    Comment da string de conexão com  banco de produção e inclusão de string para conexão com banco local
    
    Autor: cassiadeny <cassia.deny@gmail.com>
    Data: terça-feira, 20 de setembro de 2016 00:54:02
    Ajustes nos comentários do server.js
    
    Autor: cassiadeny <cassia.deny@gmail.com>
    Data: terça-feira, 20 de setembro de 2016 00:50:37
    Merge branch 'master' of https://git.heroku.com/devmeuamadonegocio
    
    Autor: cassiadeny <cassia.deny@gmail.com>
    Data: terça-feira, 20 de setembro de 2016 00:50:15
    Inclusão do terceiro post e ajustes de rota e comentários no server
    
    Autor: Rodrigo Miyashiro <rmdevwork@gmail.com>
    Data: terça-feira, 20 de setembro de 2016 00:17:32
    Email validado || instalado validator [npm install validador]

---
+++ server_dev.js
@@ -0,0 +1,139 @@
+//Declaração de variáveis
+var express = require('express'),
+	app = express(),
+	bodyParser = require('body-parser');
+	cons = require('consolidate');
+	MongoClient = require('mongodb').MongoClient,
+
+//Configurações dos pacotes do Node.js para a aplicação
+app.engine('html', cons.nunjucks);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/js'));
+
+//Definição da porta de listen do servidor
+var port = Number(process.env.PORT || 3000);
+app.listen(port);
+
+//Handle para erros internos do servidor
+function errorHandler(err, req, res, next){
+	res.status(500);
+	res.render('error_template', {error : err});
+}
+
+app.use(errorHandler);
+
+//Conexão com banco de dados local (Cássia)
+const CONNECTIONSTRING = "mongodb://localhost:27017/test";
+
+//Rotas
+app.post('/', function(req, res, next){
+	getLeads(req, res, 'index');
+});
+
+app.get('/', function(req, res, next){
+	res.render('index');
+});
+
+//============
+//Matéria 1
+//============
+app.post('/como-usar-as-metas-para-vender-mais', function(req, res, next){
+	getLeads(req, res, 'contents/como-usar-as-metas-para-vender-mais');
+});
+
+app.get('/como-usar-as-metas-para-vender-mais', function(req, res, next){
+	res.render('contents/como-usar-as-metas-para-vender-mais');
+});
+
+//============
+//Matéria 2
+//============
+app.post('/sera-que-eu-devo-aceitar-cartao', function(req, res, next){
+	getLeads(req, res, 'contents/sera-que-eu-devo-aceitar-cartao');
+});
+
+app.get('/sera-que-eu-devo-aceitar-cartao', function(req, res, next){
+	res.render('contents/sera-que-eu-devo-aceitar-cartao');
+});
+
+//============
+//Matéria 3
+//============
+app.post('/como-usar-o-excel-ao-seu-favor', function(req, res, next){
+	getLeads(req, res, 'contents/como-usar-o-excel-ao-seu-favor');
+});
+
+app.get('/como-usar-o-excel-ao-seu-favor', function(req, res, next){
+	res.render('contents/como-usar-o-excel-ao-seu-favor');
+});
+
+
+//Cadastro do visitante
+function getLeads(req, res, page){
+
+	res.render(page);
+
+	var name = req.body.name;
+	var menu = req.body.menu;
+	var ip = req.body.ip;
+	var date = new Date().toISOString();
+	const email = req.body.email;
+	const leadInfos = {'name': name, 'email': email, 'classe': menu, 'ip': ip, 'data': date};
+
+	MongoClient.connect(CONNECTIONSTRING, function(err, db) {
+		validateEmail(err, db, email, leadInfos);	
+	});
+}
+
+//Funções
+function validateEmail(err, db, email, leadInfos){
+	console.log(leadInfos);
+	console.log(email);
+
+	var validator = require('validator');
+	var isEmail = validator.isEmail(email);
+	console.log(isEmail);
+
+	if (isEmail) {
+		var query = {"email": email};
+		var cursor = db.collection('leads').find(query);
+		var inBase = false;
+
+		cursor.forEach(
+			function(doc){
+				//trocar por uma popup
+				console.log("já cadastrado!")
+				inBase = true;			
+			},
+			
+			function(err){ 
+				db.close();
+
+				if (! inBase) {
+					addLead(leadInfos);	
+				} 
+			}
+		);
+	}
+}
+
+function addLead(leadInfos){
+
+	MongoClient.connect(CONNECTIONSTRING, function(err, db) {
+
+		db.collection('leads').insertOne(leadInfos, function(err, res){
+			if (err != null){
+				console.log('Erro ao tentar inserir lead ' + err)
+			}
+
+			console.log("insert sucesso!");
+			db.close();
+		});
+	});
+}
+
+
+
